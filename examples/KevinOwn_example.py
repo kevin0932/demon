8834f19aa7d8497571f762953a64d707e65741cd
Qixuan Zhang <kevin@sans-lab-33.ethz.ch>
2017-11-22 17:35:35 +0100

    thesis modification

--- examples/KevinOwn_example.py
+++ examples/KevinOwn_example.py
@@ -11,6 +11,29 @@ sys.path.insert(0, os.path.join(examples_dir, '..', 'python'))
 
 from depthmotionnet.networks_original import *
 
+def angleaxis_to_rotation_matrix(aa):
+    """Converts the 3 element angle axis representation to a 3x3 rotation matrix
+
+    aa: numpy.ndarray with 1 dimension and 3 elements
+    Returns a 3x3 numpy.ndarray
+    """
+    if len(aa.shape) == 2:
+        # aa = np.reshape(aa, (aa.shape[1]))
+        aa = np.squeeze(aa)
+    angle = np.sqrt(aa.dot(aa))
+
+    if angle > 1e-6:
+        c = np.cos(angle);
+        s = np.sin(angle);
+        u = np.array([aa[0]/angle, aa[1]/angle, aa[2]/angle]);
+
+        R = np.empty((3,3))
+        R[0,0] = c+u[0]*u[0]*(1-c);      R[0,1] = u[0]*u[1]*(1-c)-u[2]*s; R[0,2] = u[0]*u[2]*(1-c)+u[1]*s;
+        R[1,0] = u[1]*u[0]*(1-c)+u[2]*s; R[1,1] = c+u[1]*u[1]*(1-c);      R[1,2] = u[1]*u[2]*(1-c)-u[0]*s;
+        R[2,0] = u[2]*u[0]*(1-c)-u[1]*s; R[2,1] = u[2]*u[1]*(1-c)+u[0]*s; R[2,2] = c+u[2]*u[2]*(1-c);
+    else:
+        R = np.eye(3)
+    return R
 
 def prepare_input_data(img1, img2, data_format):
     """Creates the arrays used as input from the two images."""
@@ -62,8 +85,16 @@ else: # running on cpu requires channels_last data format
 #
 
 # read data
-img1 = Image.open(os.path.join(examples_dir,'sculpture1.png'))
-img2 = Image.open(os.path.join(examples_dir,'sculpture2.png'))
+# img1 = Image.open(os.path.join(examples_dir,'P1180170_processed.JPG'))
+# img2 = Image.open(os.path.join(examples_dir,'P1180171_processed.JPG'))
+# img1 = Image.open(os.path.join(examples_dir,'P1180170.JPG'))
+# img2 = Image.open(os.path.join(examples_dir,'P1180171.JPG'))
+img1 = Image.open(os.path.join(examples_dir,'P1180151.JPG'))
+img2 = Image.open(os.path.join(examples_dir,'P1180153.JPG'))
+# img1 = Image.open(os.path.join(examples_dir,'P1180153.JPG'))
+# img2 = Image.open(os.path.join(examples_dir,'P1180151.JPG'))
+# img1 = Image.open(os.path.join(examples_dir,'PIC004.JPG'))
+# img2 = Image.open(os.path.join(examples_dir,'PIC005.JPG'))
 
 input_data = prepare_input_data(img1,img2,data_format)
 
@@ -96,13 +127,26 @@ for i in range(3):
     )
 rotation = result['predict_rotation']
 print('rotation = ', rotation)
+print(len(rotation.shape))
+print(len(rotation[:].shape))
+print('rotation matrix = ', angleaxis_to_rotation_matrix(rotation))
 translation = result['predict_translation']
 print('translation = ', translation)
+print(type(result))
+print(result.keys())
+
 result = refine_net.eval(input_data['image1'],result['predict_depth2'])
 
-
+print(type(result))
+# for k, v in result.items():
+#     print(k, v)
+print(result.keys())
 plt.imshow(result['predict_depth0'].squeeze(), cmap='Greys')
 plt.show()
+# plt.imshow(result['predict_depth_upsampled'].squeeze(), cmap='Greys')
+# plt.show()
+# plt.imshow(result['predict_depth2'].squeeze(), cmap='Greys')
+# plt.show()
 
 # try to visualize the point cloud
 try:
