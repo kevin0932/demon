12465e80826af9329e721251fb3283f8b40ba6b6
Qixuan Zhang <kevin@sans-lab-33.ethz.ch>
2018-06-14 19:11:15 +0200

    cleaning

--- thesis_scripts/framework/globalSfM_from_DeMoN_complete_pipeline.sh
+++ thesis_scripts/framework/globalSfM_from_DeMoN_complete_pipeline.sh
@@ -19,6 +19,8 @@
 #		--original_images			->	original image files in either .JPG or .png
 #		--sparse				->	COLMAP sparse results from original images (it can be used as accurate reference if COLMAP makes reasonable reconstructions)
 
+# make the following .sh scripts executable (chmod +x scriptfile) if they are not executable
+
 for dataset_name
 do
   # preprocess the input images
